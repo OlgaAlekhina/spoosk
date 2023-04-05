56b9d28f1c1abc9cf3eeaccdb239e53984eee322
EkaterinaRus <kateivan.89@mail.ru>
2023-04-05 14:51:57 +0300

    Создание views

--- spoosk/resorts/views.py
+++ spoosk/resorts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+
+from .models import SkiResort
+
+
+class SkiResortList(ListView):
+    model = SkiResort  # указываем модель, объекты которой мы будем выводить
+    template_name = 'resorts.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'resorts'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
