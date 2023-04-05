63dc911554a6617cb1e52e20ffb2e62590c5f755
EkaterinaRus <kateivan.89@mail.ru>
2023-04-05 13:08:24 +0300

    Добавление приложения resorts в проект

--- spoosk/spoosk/settings.py
+++ spoosk/spoosk/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'spoosk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spoosk',
+        'USER': 'adminkate',
+        'PASSWORD': 'adminkate',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
