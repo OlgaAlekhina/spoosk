6e9b7d6dd65fe3606532ea9fc1167e1f726af06f
Ekaterina Rusakova <kateivanrus89@gmail.com>
2023-05-12 14:34:23 +0300

    Добавление переменных окружения

--- spoosk/spoosk/prod_settings.py
+++ spoosk/spoosk/prod_settings.py
@@ -1,25 +1,25 @@
 from pathlib import Path
 import os
+from .settings import *
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-1f(irs+z-&_w@j!^a6#q_xweqv3g)dlw&jmcwvg2v#t8&k6$k!'
+# DEBUG = False
 
-DEBUG = False
+# ALLOWED_HOSTS = ["127.0.0.1"]
+#
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT')
+#     }
+# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'spoosk',
-        'USER': 'adminkate',
-        'PASSWORD': 'adminkate',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
