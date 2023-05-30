54669ac79ae9c9368e21e731f95229e22fbc55d7
Ekaterina Rusakova <kateivanrus89@gmail.com>
2023-05-30 19:48:43 +0300

    adjustment colorBars

--- spoosk/static/js/script.js
+++ spoosk/static/js/script.js
@@ -44,4 +44,30 @@ main.addEventListener('keydown', function (e) {
 // });
 
 
+/*--------------------colorBars----------------------------------*/
 
+colorBars = document.querySelectorAll('.count-trails-list');
+
+colorBars.forEach( function (bar) {
+    const b = bar.querySelectorAll('.color-bar')
+    let total_count = 0
+    const countsTrail = {}
+    for (let i = 0; i < b.length; i++) {
+        let level = b[i].classList[1]
+        let count = b[i].innerHTML
+        total_count += Number(count)
+        countsTrail[level] = count
+    };
+
+    let percentByLevel = {};
+    for (const [level, counts] of Object.entries(countsTrail)) {
+        percentByLevel[level] = Math.fround(counts / total_count * 100);
+    };
+
+    for (let i = 0; i < b.length; i++) {
+        let level = b[i].classList[1];
+        let percent = percentByLevel[level];
+        b[i].style.width = percent + '%';
+      }
+
+});
