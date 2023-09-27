1fa8b0a2f04c832f562f60e43850eb24eb75e9b9
Olga Alekhina <alekhina.olga@gmail.com>
2023-09-27 19:46:53 +0300

    add userprofile model and media folder

--- spoosk/accounts/models.py
+++ spoosk/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='/avatars/Noted_avatar.png', upload_to='avatars')
+    name = models.CharField(max_length=30, default='Username')
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    website = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
