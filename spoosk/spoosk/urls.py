26b8ec340e0b0c45e75187d24a4b31aeacc020d8
OlgaAlekhina <olga-alekhina@rambler.ru>
2023-10-14 19:03:47 +0300

    add resorts GET endpoints

--- spoosk/spoosk/urls.py
+++ spoosk/spoosk/urls.py
@@ -18,11 +18,32 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from resorts import views
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Episyche Technologies",
+        default_version='v1',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+router = routers.DefaultRouter()
+router.register(r'resorts', views.SkiResortViewset)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('resorts/', include('resorts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
