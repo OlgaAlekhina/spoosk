dd3f5a89522d4a9140f37f4c7f6ba3cc2c62940f
Olga Alekhina <alekhina.olga@gmail.com>
2023-10-24 14:01:54 +0300

    add function for adding resort to user favourites

--- spoosk/resorts/urls.py
+++ spoosk/resorts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SkiResortList, FilterResortsView, SkiResortDetailView, Search
+from .views import SkiResortList, FilterResortsView, SkiResortDetailView, Search, add_resort
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('autocomplete/', views.autocomplete, name='autocomplete'),
     path('base_searching_results/', FilterResortsView.as_view(), name='base_searching_results'),
     path('<slug>/', SkiResortDetailView.as_view(), name='resort_detail'),
+    path('add_resort/<int:pk>/', add_resort, name='add_resort'),
 ]
