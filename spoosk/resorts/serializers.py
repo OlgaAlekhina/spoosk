949fae71a4bff7b798a719c9c0603c0c82f586a7
Olga Alekhina <alekhina.olga@gmail.com>
2023-10-24 10:04:28 +0300

    add fields to resorts serializer

--- spoosk/resorts/serializers.py
+++ spoosk/resorts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import SkiResort
 
+
+# serializer for SkiResort model
 class SkiResortSerializer(serializers.ModelSerializer):
     trail_length = serializers.ReadOnlyField(source='total_length_calculation', help_text="summarized length of all resort's trails [km]")
     trail_number = serializers.ReadOnlyField(source='trail_number_count', help_text="number of all resort's trails")
