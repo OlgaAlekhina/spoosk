3f685e5276c681937f2fe0135e8a75a87c2f2868
Olga Alekhina <alekhina.olga@gmail.com>
2023-11-08 19:38:13 +0300

    finished advanced resorts filter

--- spoosk/resorts/filters.py
+++ spoosk/resorts/filters.py
@@ -20,18 +20,27 @@ class MainFilter(filters.FilterSet):
 
 
 class AdvancedFilter(filters.FilterSet):
-    # has_green_skitrail = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter value should be '1' if 'easy trails' is checked")
-    has_gondola = django_filters.Filter(field_name='skilifts__gondola', distinct=True, label="parameter value should be '1' if 'gondola skilift' is checked")
-    has_armchair = django_filters.Filter(field_name='skilifts__armchair', distinct=True, label="parameter value should be '1' if 'armchair skilift' is checked")
-    has_bugelny = django_filters.Filter(field_name='skilifts__bugelny', distinct=True, label="parameter value should be '1' if 'bugelny skilift' is checked")
-    has_travelators = django_filters.Filter(field_name='skilifts__travelators', distinct=True, label="parameter value should be '1' if 'travelator skilift' is checked")
-    # resort_month = CharFilter(field_name='list_month', lookup_expr='icontains', label="parameter values are identical to form choice fields names")
-    # resort_level = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter values should be 'green' for 'Ученик', \
-    #                                     'blue' for 'Новичок', 'red' for 'Опытный', 'black' for 'Экстремал'")
+    have_green_skitrails = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter value should be 'green' if 'easy trails' is checked")
+    have_blue_skitrails = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter value should be 'blue' if 'medium trails' is checked")
+    have_red_skitrails = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter value should be 'red' if 'difficult trails' is checked")
+    have_black_skitrails = django_filters.Filter(field_name='skytrail__complexity', distinct=True, label="parameter value should be 'black' if 'hard trails' is checked")
+    have_freeride = django_filters.Filter(field_name='freeride', distinct=True, label="parameter value should be '1' if 'freeride' is checked")
+    have_snowpark = django_filters.Filter(field_name='snowpark', distinct=True, label="parameter value should be '1' if 'snowpark' is checked")
+    have_gondola = django_filters.Filter(field_name='skilifts__gondola', distinct=True, label="parameter value should be '1' if 'gondola skilift' is checked")
+    have_armchair = django_filters.Filter(field_name='skilifts__armchair', distinct=True, label="parameter value should be '1' if 'armchair skilift' is checked")
+    have_bugelny = django_filters.Filter(field_name='skilifts__bugelny', distinct=True, label="parameter value should be '1' if 'bugelny skilift' is checked")
+    have_travelators = django_filters.Filter(field_name='skilifts__travelators', distinct=True, label="parameter value should be '1' if 'travelator skilift' is checked")
+    have_adult_school = django_filters.Filter(field_name='school', distinct=True, label="parameter value should be '1' if 'adult school' is checked")
+    have_children_school = django_filters.Filter(field_name='children_school', distinct=True, label="parameter value should be '1' if 'children school' is checked")
+    airport_distance = django_filters.Filter(field_name='distance_airport', lookup_expr='lte', distinct=True, label="parameter value should be '50' or '100' if 'not more than 50 km' or 'not more than 100 km' is checked")
+    have_rental = django_filters.Filter(field_name='equip_rental', distinct=True, label="parameter value should be '1' if 'equipment rental' is checked")
+    have_evening_skiing = django_filters.Filter(field_name='evening_skiing', distinct=True, label="parameter value should be '1' if 'evening skiing' is checked")
 
     class Meta:
         model = SkiResort
-        fields = ('has_gondola', )
+        fields = ('have_green_skitrails', 'have_blue_skitrails', 'have_red_skitrails', 'have_black_skitrails', 'have_freeride',
+                  'have_snowpark', 'have_bugelny', 'have_armchair', 'have_gondola', 'have_travelators', 'have_adult_school',
+                  'have_children_school', 'airport_distance', 'have_rental', 'have_evening_skiing')
 
 #
 # def filter_by_month(month_name):
