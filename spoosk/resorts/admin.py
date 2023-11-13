2e86fdbb721038a402852623c72d51e9f6f41f6f
OlgaAlekhina <olga-alekhina@rambler.ru>
2023-11-13 16:08:50 +0300

    add table for resorts images

--- spoosk/resorts/admin.py
+++ spoosk/resorts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SkiPass, Month, RidingLevel, SkiReview
+from .models import SkiPass, Month, RidingLevel, SkiReview, ResortImage
 from .models import SkiResort
 from .models import SkiLifts
 from .models import SkyTrail
@@ -13,4 +13,5 @@ admin.site.register(SkyTrail)
 admin.site.register(Month)
 admin.site.register(RidingLevel)
 admin.site.register(SkiReview)
+admin.site.register(ResortImage)
 
