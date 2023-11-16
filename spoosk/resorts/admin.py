b02319bb2422bfdf048368ca24d54861e1ce7957
Olga Alekhina <alekhina.olga@gmail.com>
2023-11-16 15:52:49 +0300

    add review images table

--- spoosk/resorts/admin.py
+++ spoosk/resorts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SkiPass, Month, RidingLevel, SkiReview, ResortImage
+from .models import SkiPass, Month, RidingLevel, SkiReview, ResortImage, ReviewImage
 from .models import SkiResort
 from .models import SkiLifts
 from .models import SkyTrail
@@ -14,4 +14,5 @@ admin.site.register(Month)
 admin.site.register(RidingLevel)
 admin.site.register(SkiReview)
 admin.site.register(ResortImage)
+admin.site.register(ReviewImage)
 
