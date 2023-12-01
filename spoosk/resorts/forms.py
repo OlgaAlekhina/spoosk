e15484a11ea25a3f176abee3a6db36975e3b9a54
Ekaterina Rusakova <kateivanrus89@gmail.com>
2023-12-01 16:16:20 +0300

    changed reviews

--- spoosk/resorts/forms.py
+++ spoosk/resorts/forms.py
@@ -11,9 +11,10 @@ class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = SkiReview
-        fields = ['author', 'text']
+        # fields = ['author', 'text']
+        fields = ['text']
         widgets = {
-            'author': forms.TextInput(attrs={'class': 'form__input', 'placeholder': 'Введите ваше имя'}),
+            # 'author': forms.TextInput(attrs={'class': 'form__input', 'placeholder': 'Введите ваше имя'}),
             'text': forms.Textarea(attrs={'class': 'form__text',  'placeholder': 'Введите свой отзыв здесь', 'rows': "10", 'cols': "90"}),
             # 'riding_level': forms.Select(attrs={'class': 'form__text'}),
         }
