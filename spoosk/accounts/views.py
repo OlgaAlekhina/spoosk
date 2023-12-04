771bfa1c51cc856f8b17f9936deaea167a5b7d2c
Olga Alekhina <alekhina.olga@gmail.com>
2023-12-04 19:07:04 +0300

    add function to create missing userprofiles

--- spoosk/accounts/views.py
+++ spoosk/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.http import JsonResponse, HttpResponse, Http404
 
-# Create your views here.
+
+# временная функция для создания UserProfile для ранее созданных пользователей (удалить вместе с url после однократного использования на проде)
+def add_missing_profiles(request):
+    users = User.objects.all()
+    for user in users:
+        created = UserProfile.objects.get_or_create(user=user)
+        print(user.username, ' : ', created)
+    print("all done")
+    return HttpResponse("It's done.")
+
--- spoosk/spoosk/urls.py
+++ spoosk/spoosk/urls.py
@@ -44,7 +44,7 @@ router.register(r'reviews', views.SkiReviewViewset)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    path('', RedirectView.as_view(pattern_name='resorts', permanent=True)),
+    path('', include('accounts.urls')),
     path('resorts/', include('resorts.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
