141ba271492a889b8813899d9df47fc79c2d5d31
Olga Alekhina <alekhina.olga@gmail.com>
2023-12-06 21:06:38 +0300

    remove media root

--- spoosk/spoosk/settings.py
+++ spoosk/spoosk/settings.py
@@ -136,8 +136,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_IMAGE_URL = 'static/image'
 STATIC_IMAGE_ROOT = os.path.join(BASE_DIR, 'image')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
