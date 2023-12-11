da9c2f462490d15c705fbc9a48bd5beceec4c60d
Olga Alekhina <alekhina.olga@gmail.com>
2023-12-11 20:49:15 +0300

    add endpoint for code verification

--- spoosk/spoosk/settings.py
+++ spoosk/spoosk/settings.py
@@ -192,8 +192,8 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'spoosk.info@gmail.com'
-EMAIL_HOST_USER = 'spoosk.info@gmail.com'
-EMAIL_HOST_PASSWORD = 'qpqhkyiaqcdfldrv'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
