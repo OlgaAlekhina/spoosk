43bb4bf82b138706fbf2d0744a6fc0b7a04879fb
Olga Alekhina <alekhina.olga@gmail.com>
2023-12-14 18:56:48 +0300

    change mail settings

--- spoosk/spoosk/settings.py
+++ spoosk/spoosk/settings.py
@@ -175,8 +175,8 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'spoosk.info@gmail.com'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'spoosk.info@gmail.com'
+EMAIL_HOST_PASSWORD = 'qpqhkyiaqcdfldrv'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
