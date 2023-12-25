83e87ac5bd31f2c983191f40a61e473e51710df7
Ekaterina Rusakova <kateivanrus89@gmail.com>
2023-12-25 18:28:38 +0300

    adding previews for photos in the review form

--- spoosk/static/js/script.js
+++ spoosk/static/js/script.js
@@ -44,7 +44,7 @@ main.addEventListener('keydown', function (e) {
 // });
 
 
-/*--------------------colorBars----------------------------------*/
+// colorBars
 
 colorBars = document.querySelectorAll('.count-trails-list');
 
@@ -72,9 +72,110 @@ colorBars.forEach( function (bar) {
 
 });
 
-// call function to add resort to user's favorites
-//function addtoFavorites(id_resort) {
-//    var path = window.location.href
-//    var url = "/resorts/add_resort/" + id_resort +'?next=' + path;
-//    window.location.replace(url);
-//}
+
+function byteToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (!bytes) {
+        return '0 Byte'
+    }
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+    return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
+}
+
+// uploading photos to preview in the review form
+function upload(selector, options = {}) {
+
+    let files = []
+    console.log(files)
+    const input = document.getElementById(selector)
+    const maxImages = options.maxImages || 5;
+    let currentImages = 0;
+
+    if (options.multi) {
+        input.setAttribute('multiple', true)
+    }
+
+    if (options.accept && Array.isArray(options.accept)) {
+        input.setAttribute('accept', options.accept.join(','))
+    }
+
+    const label = document.querySelector('.foto-field');
+    const fotoContainer = document.querySelector('.foto-container')
+
+    const changeHandler = event => {
+        console.log(event.target.files.length)
+        if (!event.target.files.length || currentImages > maxImages) {
+            return
+        }
+
+        files = Array.from(event.target.files)
+
+        let number = maxImages - currentImages
+
+        for (let i = 0; i < Math.min(files.length, number); i++) {
+            const file = files[i];
+
+            if (!file.type.match('image')) {
+                continue;
+            }
+
+            const reader = new FileReader()
+
+            reader.onload = ev => {
+                const src = ev.target.result
+                fotoContainer.insertAdjacentHTML('beforeend', `
+                    <div class="preview-image">
+                        <div class="preview-remove" data-name=${file.name}>&times;</div>
+                        <img src="${src}" alt="${file.name}">
+                        <div class="preview-info">
+                            ${byteToSize(file.size)}
+                        </div>
+                    </div>
+                `)
+            }
+
+            reader.readAsDataURL(file)
+
+            currentImages++;
+            if (currentImages >= maxImages) {
+                input.setAttribute('disabled', true);
+                label.style.display = 'none';
+            } else {
+                input.removeAttribute('disabled');
+                label.style.display = 'block';
+            }
+        }
+    }
+
+    const removeHandler = event => {
+        console.log('event', event.target.dataset)
+        if (!event.target.dataset.name) {
+            return
+        }
+
+        const {name} = event.target.dataset
+        files = files.filter(file => file.name !== name)
+
+        const block = fotoContainer.querySelector(`[data-name="${name}"]`).closest('.preview-image')
+
+        block.classList.add('removing')
+        setTimeout(() => block.remove(), 300)
+
+        currentImages--;
+        if (currentImages < maxImages) {
+            input.removeAttribute('disabled');
+            label.style.display = 'block';
+        }
+    }
+
+    input.addEventListener('change', changeHandler)
+    fotoContainer.addEventListener('click', removeHandler)
+}
+
+upload('foto_review', {
+    multi: true,
+    accept: ['.png', '.jpg', '.jpeg'],
+    maxImages: 5,
+})
+
+
