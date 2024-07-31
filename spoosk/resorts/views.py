e40f1f1dafc91f6764f073452c6bfb57639e26c0
olga <olga-alekhina@rambler.ru>
2024-08-01 00:47:15 +0300

    add trail_length2 field to resort's model

--- spoosk/resorts/views.py
+++ spoosk/resorts/views.py
@@ -351,7 +351,7 @@ class SkiResortList(Region, ListView):
     def get_queryset(self):
         ratings = SkiReview.objects.filter(resort=OuterRef("pk"), approved=True).order_by().values('resort').annotate(
             resort_rating=Avg('rating', output_field=FloatField())).values('resort_rating')[:1]
-        return SkiResort.objects.annotate(rating=Coalesce(Subquery(ratings), 0, output_field=FloatField())).order_by('-rating')[:6]
+        return SkiResort.objects.annotate(rating=Coalesce(Subquery(ratings), 0, output_field=FloatField())).order_by('-rating').order_by('name')[:6]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
